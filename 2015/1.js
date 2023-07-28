fee3535b476e0405ba9ae8a5bfd9779c0b49e3ba
RobotFK <31999284+RobotFK@users.noreply.github.com>
2023-07-28 14:34:43 +0200

    Laying Groundwork

--- 2015/1.js
+++ 2015/1.js
@@ -1,3 +1,16 @@
-function f() {
+var Day_1_Input
+document.getElementById('Day_1_Input').onchange = () => {
+    const file = document.getElementById('Day_1_Input').files[0];
+	if (file) {
+        const reader = new FileReader();
+        reader.readAsText(file, 'UTF-8');
+		reader.onload = (evt) => {
+          //console.log(evt.target.result);
+          Day_1_Input = evt.target.result
+        };
+    }
+}
+
+function Day_1_1() {
   document.getElementById("output").innerHTML = "Executed";
 }
