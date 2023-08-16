4bedcb3c6f18bea3b83f833aadcf01589dd00a47
RobotFK <31999284+RobotFK@users.noreply.github.com>
2023-08-16 11:22:44 +0200

    Added Visuals for Day 1-1

--- 2015/1.js
+++ 2015/1.js
@@ -13,17 +13,60 @@ document.getElementById('Day_1_Input').onchange = () => {
     }
 };
 
+const heights = [];
 function Day_1_1() {
 	if (Day_1_Input == undefined) {return}
+	if (heights.length > 100) {return}
 	let final_height = 0;
-	const heights = [];
+	//const heights = [];
 	for (let i = 0; i < Day_1_Input.length ; i++){
 		if (Day_1_Input[i] == "("){final_height += 1}
 		if (Day_1_Input[i] == ")"){final_height -= 1}
 		heights.push({x:i,y:final_height}); 
 	}
-	
-	document.getElementById("output").innerHTML = String(final_height);
+	let node =  document.getElementById('visualisation');
+	while (node.hasChildNodes()) {
+    node.removeChild(node.lastChild);
+	}
+	//if(document.getElementById('visualisation').childNodes.length >= 3){return}
+	const canvas = document.createElement('canvas');
+	canvas.id = 'chart';
+	canvas.style.border = '1px solid black';
+	canvas.height = '500';
+	canvas.width = '1500';
+	document.getElementById('visualisation').appendChild(canvas);
+	{
+		var chartinput = {
+			datasets: [{
+				data: heights
+			}],
+			labels: [...Array(heights.length).keys()] 
+			}
+		console.log(chartinput);
+		new Chart(
+		document.getElementById('chart'),
+		{
+		  type: 'line',
+		  data: chartinput,
+		  options: {
+			parsing: {
+			  xAxisKey: 'x',
+			  yAxisKey: 'y'
+			},
+			plugins: {
+			  legend: {
+				position: 'top',
+				},
+			  title: {
+				display: true,
+				text: 'Height of Santa'
+				}
+			},
+		  }
+		}
+		);
+	}
+	document.getElementById("result").innerHTML = "Final height is at: "+ String(final_height);
 }
 
 function Day_1_2() {
@@ -40,5 +83,5 @@ function Day_1_2() {
 		}
 	}
 	
-	document.getElementById("output").innerHTML = String(final_pos);
+	document.getElementById("result").innerHTML = String(final_pos);
 }
