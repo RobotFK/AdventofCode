71a1756948fd73aafc451c2e53d7f88f2fe72d42
RobotFK <31999284+RobotFK@users.noreply.github.com>
2023-08-16 14:57:32 +0200

    Fixes and despair

--- 2015/1.js
+++ 2015/1.js
@@ -13,36 +13,48 @@ document.getElementById('Day_1_Input').onchange = () => {
     }
 };
 
-const heights = [];
+function clear_output(){
+	let node =  document.getElementById('visualisation');
+	while (node.hasChildNodes()) {
+	node.removeChild(node.lastChild);
+	}
+	node = document.getElementById('result');
+	while (node.hasChildNodes()) {
+	node.removeChild(node.lastChild);
+	}
+	return true
+}
+
+
+const heights = []; //Global to allow for debugging
 function Day_1_1() {
 	if (Day_1_Input == undefined) {return}
-	if (heights.length > 100) {return}
 	let final_height = 0;
-	//const heights = [];
+	const heights = [];
+	if (heights.length < 100){//first Calc
 	for (let i = 0; i < Day_1_Input.length ; i++){
 		if (Day_1_Input[i] == "("){final_height += 1}
 		if (Day_1_Input[i] == ")"){final_height -= 1}
 		heights.push({x:i,y:final_height}); 
+	}}else{
+		final_height = heights[heights.length -1].y 
 	}
-	let node =  document.getElementById('visualisation');
-	while (node.hasChildNodes()) {
-    node.removeChild(node.lastChild);
-	}
-	//if(document.getElementById('visualisation').childNodes.length >= 3){return}
+	clear_output();
 	const canvas = document.createElement('canvas');
 	canvas.id = 'chart';
 	canvas.style.border = '1px solid black';
 	canvas.height = '500';
 	canvas.width = '1500';
 	document.getElementById('visualisation').appendChild(canvas);
-	{
+	{ //Graph Placement
 		var chartinput = {
 			datasets: [{
+				lable: 'height',
 				data: heights
 			}],
 			labels: [...Array(heights.length).keys()] 
 			}
-		console.log(chartinput);
+		//console.log(chartinput);
 		new Chart(
 		document.getElementById('chart'),
 		{
@@ -54,12 +66,12 @@ function Day_1_1() {
 			  yAxisKey: 'y'
 			},
 			plugins: {
-			  legend: {
-				position: 'top',
+				legend: {//Not sure how to set this 
+				display: false,
 				},
-			  title: {
-				display: true,
-				text: 'Height of Santa'
+				title: {
+					display: true,
+					text: 'Height of Santa'
 				}
 			},
 		  }
@@ -82,6 +94,5 @@ function Day_1_2() {
 			break;
 		}
 	}
-	
-	document.getElementById("result").innerHTML = String(final_pos);
+	document.getElementById("result").innerHTML = "The first Postion at witch he entered the basement is at: " + String(final_pos);
 }
