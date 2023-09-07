c4c367120290141d96609093f04dc04d6dbe29f8
RobotFK <31999284+RobotFK@users.noreply.github.com>
2023-09-07 09:26:33 +0200

    Add files via upload

---
+++ 2015/5-1-2.ps1
@@ -0,0 +1,12 @@
+﻿cd C:\Daten\2015
+
+$myinput = Get-Content -Path .\5-1.txt
+#$myinput = "jchzalrnumimnmhp"
+$sum = 0 
+
+$myinput |%{if(($_ -match '((a|e|i|o|u)\S*){3}') -and ($_ -match '([a-z])\1')-and ($_ -notmatch '(ab)|(cd)|(pq)|(xy)')){$sum +=1}}
+
+$sum
+
+#$test = "jchzalrnumimnmhp"
+#if(($test -match '((a|e|i|o|u)\S*){3}') -and ($test -match '([a-z])\1')-and ($test -notmatch '(ab)|(cd)|(pq)|(xy)')){"True"}
