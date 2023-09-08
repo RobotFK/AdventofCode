9f73fd24e24d30d331ea1807e55b6af1cf8bb2f5
RobotFK <31999284+RobotFK@users.noreply.github.com>
2023-09-08 14:26:44 +0200

    Add files via upload

---
+++ 2015/10-1.ps1
@@ -0,0 +1,29 @@
+﻿cd C:\Daten\2015
+#Not Terrebly fast, but does it's job
+$myinput = "1113122113"
+
+function Expand-Lookandsay{
+param(
+    [string]$prev
+) 
+ $running_digit = $prev[0]
+ $running_amount = 0
+ 0..($prev.Length-1)|%{
+    $digit = $prev[$_]
+    #Write-Host "$($digit) -eq $($running_digit) : $($digit -eq $running_digit)"
+    if($digit -eq $running_digit){
+        $running_amount++
+    }else{ 
+        $next += "$($running_amount)$($running_digit)"
+        $running_digit = $digit
+        $running_amount = 1
+    }
+ }
+ $next += "$($running_amount)$($running_digit)"
+ return $next
+}
+
+$result = $myinput
+1..40|%{$result = Expand-Lookandsay $result;Write-Host $_}
+
+Write-Host $result.length
