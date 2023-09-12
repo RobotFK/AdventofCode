99defc50714fd566d3ed6198647810a7eac7c19a
RobotFK <31999284+RobotFK@users.noreply.github.com>
2023-09-12 08:39:36 +0200

    Add files via upload

---
+++ 2015/12-1.ps1
@@ -0,0 +1,11 @@
+﻿cd C:\Daten\2015
+
+$myinput = Get-Content .\12-1.txt -Raw | ConvertFrom-Json #I can feel this being usefull later
+$myinput = Get-Content .\12-1.txt
+
+$list = $myinput -replace "[^-|\d]+", ","
+
+$sum = 0
+$list.Split(",")|%{$sum += $_}
+
+$sum
