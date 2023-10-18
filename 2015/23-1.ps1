5d497035745ffc7ae712db4275f56cc3992d13e2
RobotFK <31999284+RobotFK@users.noreply.github.com>
2023-10-18 07:33:02 +0200

    Add files via upload

---
+++ 2015/23-1.ps1
@@ -0,0 +1,26 @@
+cd C:\Daten\2015
+
+$myinput = Get-Content -Path .\23-1.txt
+
+$registers = @{
+a = 0;
+b = 0;
+}
+
+$instructions = $myinput
+$instructionindex = 0
+
+while($instructionindex -lt $instructions.Length){
+    #Write-host $instructionindex
+    $instruction = $instructions[$instructionindex].Replace(',','').split()
+    switch ($instruction[0]){
+    'hlf'{$registers.($instruction[1])/=2;$instructionindex++}
+    'tpl'{$registers.($instruction[1])*=3;$instructionindex++}
+    'inc'{$registers.($instruction[1])++;$instructionindex++}
+    'jmp'{$instructionindex += $instruction[1]}
+    'jie'{if($registers.($instruction[1])%2 -eq 0){$instructionindex += $instruction[2]}else{$instructionindex++}}
+    'jio'{if($registers.($instruction[1]) -eq 1){$instructionindex += $instruction[2]}else{$instructionindex++}}
+    }
+}
+
+Write-host "B: $($registers.b)"
